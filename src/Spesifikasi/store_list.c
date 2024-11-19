922519101558e5bd7b41c7814cad1fd751fbbe33
18223052 <18223052@std.stei.itb.ac.id>
2024-11-19 20:12:54 +0700

    feat main

--- src/Spesifikasi/store_list.c
+++ src/Spesifikasi/store_list.c
@@ -1,4 +1,23 @@
 #include "store_list.h"
-#include <stdio.h>
-#include <stdlib.h>
+#include "store.h"
 
+int main() {
+    Store myStore;
+    initStore(&myStore, 10);
+
+    // Tambahkan barang ke toko
+    insertLast(&(myStore.barang), "Platypus Laser");
+    insertLast(&(myStore.barang), "Shrink Ray");
+    insertLast(&(myStore.barang), "Net Shooter");
+
+    // Tampilkan barang di toko
+    printf(">> STORE LIST\n");
+    displayStore(myStore);
+
+    // Toko kosong
+    dealocateList(&(myStore.barang));
+    printf("\n>> STORE LIST\n");
+    displayStore(myStore);
+
+    return 0;
+}
