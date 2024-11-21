e8ebcfb2e60d79dd8f142c53f2f029f8218a5021
Muhamad Hasbullah Faris <18223014@std.stei.itb.ac.id>
2024-11-21 11:08:48 +0700

    fix feat quit
    fix quit

--- src/Spesifikasi/quit.c
+++ src/Spesifikasi/quit.c
@@ -1,23 +1,19 @@
-#include "save.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include "quit.h"
 
 void quit(char *filename, Barang barang[], int jumlahBarang, User user[], int jumlahUser){
     printf(">>>> QUIT\n");
     printf("Apakah kamu ingin menyimpan data sesi sekarang (Y/N)? \n");
 
-    int input;
+    char input;
     printf(">> ");
-    scanf("%d", &input);
+    scanf(" %c", &input);
 
     if (input == 'Y' || input == 'y') {
-        Barang barang[MAX_BARANG];
-        User user[MAX_USER];
-        int jumlahBarang, jumlahUser;
-
-        Save("coba.txt", barang, jumlahBarang, user, jumlahUser);
-        printf("Data berhasil disimpan!\n");
+        Save(filename, barang, jumlahBarang, user, jumlahUser);
     } else if (input == 'N' || input == 'n'){
-        printf("Kamu keluar dari PURRMART.\n");
+        printf("Kamu keluar dari PURRMART.\n\n");
     }
     printf("Dadah ^_^/\n");
     exit(0);
