66dd9aa98637b0bba18c515901286ebd1fea74f5
Muhamad Hasbullah Faris <18223014@std.stei.itb.ac.id>
2024-12-19 14:12:47 +0700

    feat history

---
+++ src/Spesifikasi/history.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "history.h"
+
+void history(Stack *S) {
+    if (IsEmptyStack(*S)) {
+        printf("Kamu belum membeli barang apapun!\n");
+        return;
+    }
+    printf("\nRiwayat pembelian barang:\n");
+    for (int i = 0; i < S->TOP; i++) {
+        printf("%d. %s %d\n", 
+               i+1, 
+               S->T[i].name, 
+               S->T[i].harga);
+    }
+}
---
+++ src/Spesifikasi/history.h
@@ -0,0 +1,8 @@
+#include "../ADT/boolean.h"
+#include "../ADT/stack.h"
+
+#define MAX_LEN 50
+#define MAX_USERS 100
+
+void history(Stack *S);
+ 
