9dd8d9a1b7ad03e2840edba9adcf96a91a77d5c4
Muhamad Hasbullah Faris <18223014@std.stei.itb.ac.id>
2024-12-20 10:51:43 +0700

    feat cart show

---
+++ src/Spesifikasi/cart_show.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "cart_show.h"
+
+void cartShow(User *currentUser){
+    if (IsMapEmpty(currentUser->keranjang)){
+        printf("Keranjang kosong!\n");
+        return;
+    }
+
+    int cost = 0;
+    printf("Isi keranjang belanja:\n");
+    printf("Qty       Nama    Harga\n");
+
+    for (int i = 0; i < currentUser->keranjang.Count; i++){
+        char * itemName; copyString(currentUser->keranjang.Elements[i].Key, itemName, stringLen(currentUser->keranjang.Elements[i].Key));
+        int itemQty = currentUser->keranjang.Elements[i].Value;
+        int itemPrice = Ad.A[SearchArrayDin(Ad, itemName)].price;
+        int totalItemPrice = itemQty * itemPrice;
+
+        printf("%-10d %-7s %d\n", itemQty, itemName, totalItemPrice);
+        cost += totalItemPrice;
+    }
+
+    printf("Total belanja: %d\n", cost);
+}
---
+++ src/Spesifikasi/cart_show.h
@@ -0,0 +1,12 @@
+#ifndef __CARTSHOW_h__
+#define __CARTSHOW_h__
+
+#include <stdio.h>
+#include "../ADT/boolean.h"
+#include "../ADT/map.h"
+#include "../ADT/arrayDin.h"
+#include "load.h"
+
+void cartShow(User *currentUser);
+
+#endif
